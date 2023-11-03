b89b9b3471cd88fb95c8df09176ed40012cd7e4b
avbogao <88630887+avbogao@users.noreply.github.com>
2023-11-03 16:10:55 -0300

    casi listo el 1

--- ailen.R
+++ ailen.R
@@ -2,7 +2,7 @@ library(tidyr)
 library(tidyverse)
 library(ggplot2)
 load("tp2.RData")
-
+library(Metrics)
 # Agregamos columnas que faltan (ignoramos feriados) TODO: tener en cuenta feriados
 clima_ecobici = clima_ecobici %>% mutate(llueve = prcp > 0) %>% mutate(dia_semana = weekdays(date)) %>% 
   mutate(dia_laborable = dia_semana != 'sábado' & dia_semana != 'domingo' & date != '2022-02-28' &
@@ -18,8 +18,8 @@ clima_ecobici = clima_ecobici %>% mutate(llueve = prcp > 0) %>% mutate(dia_seman
            date != '2022-10-10' &
            date != '2022-11-21' &
            date != '2022-12-08' &
-           date != '2022-12-09' ) %>% mutate(mes = months(date)) %>% 
-mutate(categoria_temp_promedio = cut(tavg, breaks=c(-Inf, 13,22,27,Inf), labels = c('Menor a 13°','13° a 22°', '22° a 27°', 'Mayor a 27°')))
+           date != '2022-12-09' ) %>% 
+  mutate(categoria_temp_promedio = cut(tavg, breaks=c(-Inf, 13,22,27,Inf), labels = c('Menor a 13°','13° a 22°', '22° a 27°', 'Mayor a 27°')))
 
 temperatura_mensual = clima_ecobici %>%
   group_by(mes)%>%
@@ -73,18 +73,6 @@ ggplot(clima_ecobici, aes(x = tmax, y = n)) + geom_point() + labs(title = "Usos 
 
 ## Usos vs dos variables
 
-#Cantidad de usos de bicicletas segun si es día laborable o no y temperatura promedio'
-
-clima_ecobici %>%
-  ggplot(aes(x=categoria_temp_promedio, y=(n/100)))+
-  facet_grid(~dia_laborable)+
-  geom_bar(stat = 'identity')+
-  labs(title = 'Cantidad de usos de bicicletas segun si es día laborable o no y temperatura promedio', x = 'Temperatura promedio del día de uso', y = 'Cantidad de usos', subtitle = 'Escala de usos: 1:100')+
-  theme(plot.title=element_text(size=14,face="bold"))+
-  theme(plot.subtitle=element_text(size=8,face='italic'))+
-  theme(axis.title=element_text(size=10))
-
-
 # lluvia vs dia laborable
 ggplot(clima_ecobici, aes(x = dia_laborable, y = n, fill = llueve)) + geom_col() + labs(title = "Usos segun dia laborable o no laborable y lluvia", y = "Cantidad de usos")
 
@@ -117,3 +105,44 @@ ggplot(clima_ecobici, aes(x = wspd, y = n)) + geom_point(aes(color=dia_laborable
 
 
 ## usamos lluvia y dia laborable
+
+clima_ecobici %>%
+  ggplot(aes(x=categoria_temp_promedio, y=(n/100)))+
+  facet_grid(~dia_laborable)+
+  geom_bar(stat = 'identity')+
+  labs(title = 'Cantidad de usos de bicicletas segun si es día laborable o no y temperatura promedio', x = 'Temperatura promedio del día de uso', y = 'Cantidad de usos', subtitle = 'Escala de usos: 1:100')+
+  theme(plot.title=element_text(size=14,face="bold"))+
+  theme(plot.subtitle=element_text(size=8,face='italic'))+
+  theme(axis.title=element_text(size=10))
+
+
+### Modelo de regresion lineal multiple
+
+# Creacion de datasets de entrenamiento y de prueba
+set.seed(22)
+sample = sample(c(TRUE, FALSE), nrow(clima_ecobici), replace = T,prob=c(0.8,0.2))
+train  <- clima_ecobici[sample, ]
+test   <- clima_ecobici[!sample, ]
+
+# Modelo de regresion lineal (Entrenamiento)
+model = lm(n ~ dia_laborable + tavg, train)
+summary(model)
+b0 = coef(model)[1]
+b1 = coef(model)[2]
+b2 = coef(model)[3]
+# vemos que tan bien predice 
+test$usos_pred = predict(model, test, type = "response")
+
+# error cuadratico medio, y el r cuadrado
+rmse(test$n, test$usos_pred)
+summary(model)$r.squared # es un buen modelo
+# The lowest R-squared is 0 and means that the points are not explained by the regression whereas the highest R-squared is 1 and 
+# means that all the points are explained by the regression line
+
+usos_pred = predict(model, clima_ecobici, type = "response")
+## Graficos
+ggplot(clima_ecobici, aes(x = tavg, y = n)) + geom_point(aes(color=dia_laborable)) + 
+  labs(title = "Usos segun temperatura promedio y dia laborable", y = "Cantidad de usos", x = "Temperatura promedio (ºC)") + 
+  geom_abline(intercept = b0 , slope = b2 , color = "red") + # dia_laborable == FALSE
+  geom_abline(intercept = b0+b1 , slope = b2 , color = "red") # dia_laborable == TRUE
+
